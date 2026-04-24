96d02504ae5bbfec06f348b1c588c9e86035c1ed
Devyanshkumarshah <144312351+Devyanshkumarshah@users.noreply.github.com>
2026-04-24 22:34:18 +0530

    Update Gateway_Main.c
    Added comment to clarify LORA_TX_POWER definition.

--- Gateway_Main.c
+++ Gateway_Main.c
@@ -36,7 +36,7 @@ static LoRa_HandleTypeDef lora = {
 #define LORA_SF         7
 #define LORA_BW         125000UL
 #define LORA_CR         5
-#define LORA_TX_POWER   17
+#define LORA_TX_POWER   17            // 17 dB out of 24 dB
 #define LORA_SYNC_WORD  0x34
 
 #define PKT_LEN         10
